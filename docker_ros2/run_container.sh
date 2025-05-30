7ac1fbba2e3c6ef843de8aee5dcfc53f26f6619c
Elzaaa <elza.shakirova1307@gmail.com>
2025-05-30 14:46:39 -0700

    Remove ROS 2 Docker setup (project is ROS 1 only)

--- docker_ros2/run_container.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-docker run -it --rm --name ros-humble \
-  --net=host \
-  --privileged \
-  -e RMW_IMPLEMENTATION=rmw_fastrtps_cpp \
-  -v ~/swarm-formation-ros:/home/ros-user/swarm-formation-ros \
-  ros-humble-img
